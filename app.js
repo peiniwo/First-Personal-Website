317f62ff428b8a3539d1019d176e36662d3b0a52
cwhwang1986 <cwhwang1986@gmail.com>
2016-11-20 22:17:55 -0800

    Modify the request handler

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    response.send('Hello World!')
-});
-
 app.get('/:name', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 app.get('/page/:name', function (req, res) {
     var pageName = req.params.name;
-    res.sendFile(path.join(__dirname + '/public/component-html/' + pageName + '.html'));
+    res.sendFile(path.join(__dirname +'/public/component-html/' + pageName + '.html'));
 });
 
 app.listen(process.env.PORT  || 3000, function () {
