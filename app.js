4fbf16f0c7d6e7f945da43c420b7130a14ff2c0a
cwhwang1986 <cwhwang1986@gmail.com>
2016-11-20 21:55:27 -0800

    Modify index html

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
     response.send('Hello World!')
 });
